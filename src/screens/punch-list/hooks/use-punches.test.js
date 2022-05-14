d2ccffb1c9383e4cdadab5423e0ac20081736994
Alencar Oliveira <alencar@intellimize.com>
2022-05-13 21:54:32 -0300

    add tests for usePunches hook

--- src/screens/punch-list/hooks/use-punches.test.js
+++ src/screens/punch-list/hooks/use-punches.test.js
@@ -1,43 +1,66 @@
-import { renderHook } from "@testing-library/react-native";
-import { act } from "react-test-renderer";
+import { act, renderHook } from "@testing-library/react-hooks";
+import { mocksPunchesList } from "../../../test/mocks/punchList";
 import usePunches from "./use-punches";
 
+const setup = () => renderHook(() => usePunches());
+
 describe("usePunches", () => {
-  describe("When is deleted some punch", () => {
-    it("returns updated punches length", () => {
-      const { result } = renderHook(() => usePunches());
+  describe("punches", () => {
+    it("it should returns an list of punches", () => {
+      const { result } = setup();
 
-      expect(result.current.punches).toHaveLength(4);
-
-      act(() => {
-        result.current.deletePunch(1);
-      });
-
-      expect(result.current.punches).toHaveLength(3);
+      expect(result.current.punches).toEqual(mocksPunchesList);
     });
   });
+});
 
-  describe("When is restored a removed punch", () => {
-    it("returns punches with restored punch at same index position", () => {
-      const { result } = renderHook(() => usePunches());
+describe("deletePunches", () => {
+  it("it should delete a punch with given Id", () => {
+    const { result } = setup();
+    const initialPunchesList = [...result.current.punches];
 
-      const punchRemovedIndex = 3;
-      const punchRemoved = result.current.punches[punchRemovedIndex];
+    const punchId = 1;
 
-      act(() => {
-        result.current.deletePunch(punchRemoved.id);
-      });
-
-      act(() => {
-        result.current.restorePunch({
-          index: punchRemovedIndex,
-          punchRemoved,
-        });
-      });
-
-      const restoredPunch = result.current.punches[punchRemovedIndex];
-
-      expect(restoredPunch).toStrictEqual(punchRemoved);
+    act(() => {
+      result.current.deletePunch(punchId);
     });
+
+    expect(initialPunchesList).not.toEqual(result.current.punches);
+  });
+
+  it("returns updated punches length", () => {
+    const { result } = renderHook(() => usePunches());
+
+    expect(result.current.punches).toHaveLength(4);
+
+    act(() => {
+      result.current.deletePunch(1);
+    });
+
+    expect(result.current.punches).toHaveLength(3);
+  });
+});
+
+describe("When is restored a removed punch", () => {
+  it("returns punches with restored punch at same index position", () => {
+    const { result } = renderHook(() => usePunches());
+
+    const punchRemovedIndex = 3;
+    const punchRemoved = result.current.punches[punchRemovedIndex];
+
+    act(() => {
+      result.current.deletePunch(punchRemoved.id);
+    });
+
+    act(() => {
+      result.current.restorePunch({
+        index: punchRemovedIndex,
+        punchRemoved,
+      });
+    });
+
+    const restoredPunch = result.current.punches[punchRemovedIndex];
+
+    expect(restoredPunch).toStrictEqual(punchRemoved); 
   });
 });
